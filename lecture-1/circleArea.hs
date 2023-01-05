31bc4ea4319f27a8cfdefc563bdee6e41daad1dd
Milco Numan <milco.numan@gmail.com>
2023-01-05 17:24:55 +0100

    Simple examples

---
+++ lecture-1/circleArea.hs
@@ -0,0 +1,4 @@
+circleArea :: Double -> Double
+circleArea r = pi * rsquare
+    where pi = 3.14159265
+          rsquare = r * r
---
+++ lecture-1/greet.hs
@@ -0,0 +1,6 @@
+greet :: String -> String -> String
+greet "Finland" name = "Hei, " ++ name
+greet "Italy"   name = "Ciao, " ++ name
+greet "Holland" name = "Hallo " ++ name ++ "!"
+greet "England" name = "How do you do, " ++ name
+greet _         name = "Hello, " ++ name
